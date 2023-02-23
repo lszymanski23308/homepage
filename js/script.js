ce936fba2acc747bc40e940fe167204ade3355bd
Lukasz Szymanski <lszymanski23308@gmail.com>
2023-02-23 14:45:02 +0100

    added welcome text in console.log

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("Cześć");
+console.log("Cześć Ten kod jest juz w repozytorium Git.");
 
 let button = document.querySelector(".container__button");
 let body = document.querySelector(".body");
