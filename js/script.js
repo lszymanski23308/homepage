97f4bfce5e4ea8408b54ad3705b7653359eed4f7
Lukasz Szymanski <lszymanski23308@gmail.com>
2023-02-23 14:35:02 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("Cześć");
+
+let button = document.querySelector(".container__button");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".container__button");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("light");
+
+    themeName.innerText = body.classList.contains("light") ? "Ciemny motyw" : "Jasny motyw";
+})
